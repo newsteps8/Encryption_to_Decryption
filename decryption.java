71e7af6dd1643c261a15cac0a762d8ef876284c1
Büşra Gökmen <37721917+newsteps8@users.noreply.github.com>
2019-09-23 15:20:56 +0300

    Update decryption.java

--- decryption.java
+++ decryption.java
@@ -1,14 +1,6 @@
 public class decryption {//to decryption given file
 
-  //dosyadan karakteri okuycak
-  //  okuduğu karaktere y değişkenine atıycaz
-    //a nın tersini bulcaz
-    //x= a nın tersi(y-b)mod 26  , x =asıl metindeki karakterin integer değeri olacak
-    //daha sonra x in değerini ekrana yazdırcaz
-    //a yı ve b yi bulmak için belirli değerleri deneyebiliriz
-    //a nın tersi için a * a nın tersi = 1 mod(26)
-    //a nın tersi 0 ve 26 arasında olmalı
-
+  
 
 
 
